5dc8a730d0fbed4c00a7077d2fc7f9c6670a3100
Dennis Rösner <dennis@xn--rsner-jua.de>
2019-09-15 02:11:41 +0200

    Auto-generated commit

--- ngx-toast-it/ngx-toast-it.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public-api';
-export { ComponentInjectorService as ɵd } from './lib/general/component-injector.service';
-export { ProgressBarComponent as ɵc } from './lib/progress-bar/progress-bar.component';
-export { ToastContainerComponent as ɵb } from './lib/toast-container/toast-container.component';
-export { ToastComponent as ɵa } from './lib/toast/toast.component';
